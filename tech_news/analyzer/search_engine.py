fefeb9911559c4edfcde94d175b7428a8db4f102
Caio <caioqs.info@gmail.com>
2022-02-16 09:55:44 -0300

    Req 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from ..database import search_news
+from datetime import datetime
 
 
 # Requisito 6
@@ -13,6 +14,14 @@ def search_by_title(title):
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+        print(date)
+        news = search_news({"timestamp": {"$regex": date}})
+        news_list = [(new["title"], new["url"]) for new in news]
+        return news_list
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
