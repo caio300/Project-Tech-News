1cdf24eea4866042eed1aa4a3f960afbba2de33c
Caio <caioqs.info@gmail.com>
2022-02-16 09:39:08 -0300

    Req 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,13 @@
+from ..database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    # https://stackoverflow.com/questions/7101703/how-do-i-make-case-insensitive-queries-on-mongodb/36916270#36916270
+    news = search_news({"title": {"$regex": title, "$options": "i"}})
+    new_list = [(new["title"], new["url"]) for new in news]
+    return new_list
 
 
 # Requisito 7
